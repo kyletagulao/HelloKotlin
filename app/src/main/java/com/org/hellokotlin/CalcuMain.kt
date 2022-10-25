2a6a9736ee5971af63b9c65d3e9d1bcdada90241
kyletagulao <20-03001@g.batstate-u.edu.ph>
2022-10-25 12:56:05 +0800

    V2

---
+++ app/src/main/java/com/org/hellokotlin/CalcuMain.kt
@@ -0,0 +1,86 @@
+package com.org.hellokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_calcu_main.*
+
+class CalcuMain : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar?.hide();
+
+        setContentView(R.layout.activity_calcu_main)
+
+
+        calculateButton.setOnClickListener{
+            validate(binaryInputUser.text.toString())
+        }
+
+
+
+    }
+
+    private fun validate(userNumber: String) {
+
+            octalCalculator(userNumber.toLong())
+            decimalCalculator(userNumber.toLong())
+            hexCalculator(userNumber.toLong())
+
+    }
+
+    private fun octalCalculator(binaryInputUser: Long) {
+
+        var userNumber = binaryInputUser
+        var octalNumber = 0
+        var decimalNumber = 0
+        var i = 0
+
+        while (userNumber.toInt() != 0) {
+            decimalNumber += (userNumber % 10 * Math.pow(2.0, i.toDouble())).toInt()
+            ++i
+            userNumber /= 10
+        }
+
+        i = 1
+
+        while (decimalNumber != 0){
+            octalNumber += decimalNumber %8 * i
+            decimalNumber /= 8
+            i *= 10
+        }
+
+        octalResult.text = octalNumber.toString()
+    }
+
+    private fun decimalCalculator(binaryInputUser: Long) {
+
+        var userNumber = binaryInputUser
+        var decimalNumber = 0
+        var i = 0
+        var remainder: Long
+
+        while (userNumber.toInt() != 0) {
+            remainder = userNumber % 10
+            userNumber /= 10
+            decimalNumber += (remainder * Math.pow(2.0, i.toDouble())).toInt()
+            ++i
+        }
+
+        decimalResult.text = decimalNumber.toString()
+    }
+
+    private fun hexCalculator(userNumber: Long) {
+
+
+
+    }
+
+
+
+}
--- app/src/main/java/com/org/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/org/hellokotlin/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.org.hellokotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportActionBar?.hide();
+
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, CalcuMain::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
